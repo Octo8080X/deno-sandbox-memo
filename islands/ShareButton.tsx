a5d2c5d9588f5cffd5f908aa5d1ef794380eae61
Octo8080 <okutannderuta0529@gmail.com>
2025-12-29 13:48:52 +0900

    update

--- islands/ShareButton.tsx
+++ islands/ShareButton.tsx
@@ -3,9 +3,9 @@ export default function ShareButton() {
   const handleShare = () => {
     const text = encodeURIComponent("Deno Sandbox Memo by git!\n");
     const url = encodeURIComponent(
-      "https://deno-sandbox-api-test.octo8080x.deno.net/",
+      "https://deno-sandbox-memo.octo8080x.deno.net/",
     );
-    const hashtags = encodeURIComponent("Deno,sandbox,BabylonJS");
+    const hashtags = encodeURIComponent("Deno,sandbox,git");
     globalThis.open(
       `https://twitter.com/intent/tweet?text=${text}&url=${url}&hashtags=${hashtags}`,
       "_blank",
